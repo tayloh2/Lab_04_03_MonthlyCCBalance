8b6474189a98dc33eed5cc21fefb430b46d7296f
tayloh2 <tayloh2@mail.uc.edu>
2024-06-10 22:17:19 -0400

    Lab_04_03_MonthlyCCBalance.iml

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double creditCardBalance = 5000;
+        double balanceOneMonth = creditCardBalance * 0.17;
+        double balanceTwoMonth = balanceOneMonth * 0.17;
+        System.out.println("Your interest due after one month is " + balanceOneMonth + " and your interest due after two months is " + balanceTwoMonth);
+        }
+    }
